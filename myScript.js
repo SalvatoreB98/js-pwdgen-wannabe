8d1511475a973957f63e16c6695d00bc911fa434
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-04-07 15:15:21 +0200

    First update

---
+++ myScript.js
@@ -0,0 +1,7 @@
+var nome;
+var cognome;
+var colorePreferito;
+nome = prompt("Qual'è il tuo nome?");
+cognome = prompt("Qual'è il tuo cognome?");
+colorePreferito= prompt("Qual'è il tuo colore preferito?");
+document.getElementById("pw-suggested").innerHTML=nome + cognome + "21";
